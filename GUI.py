a516192715690bc922dd0516fc171e974e49be80
idevneo <idevneo@gmail.com>
2020-10-18 14:54:30 -0500

    shooting accuracy calcs, i messed up last commit

--- GUI.py
+++ GUI.py
@@ -4,7 +4,7 @@ import csv
 
 # You'll need to install the module called Pillow
 # do "pip install Pillow"
-
+from numpy import double
 root = Tk()
 highGoal = []
 midGoal = []
@@ -49,3 +49,8 @@ canvas.pack()
 canvas.create_image(0, 0, image=goal, anchor=NW)
 
 root.mainloop()
+totalMakes = len(highGoal) + len(midGoal) + len(lowGoal)
+totalShots = len(miss) + totalMakes
+shootingAccuracy = (double) (totalMakes/(totalShots))
+
+print("Shooting Accuracy: " + "{:.2%}".format(shootingAccuracy))
